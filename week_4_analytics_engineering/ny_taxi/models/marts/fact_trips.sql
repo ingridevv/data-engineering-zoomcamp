2d2090cfff69858a505e8a03f142d6d149cebe60
Ingrid da Silva Lopes Costa <personnel.ingrid@gmail.com>
2026-02-20 11:35:56 +0000

    arch: project modularity and dim_vendors enrichment

--- week_4_analytics_engineering/ny_taxi/models/marts/fact_trips.sql
+++ week_4_analytics_engineering/ny_taxi/models/marts/fact_trips.sql
@@ -4,21 +4,10 @@
     )
 }}
 
-with green_tripdata as (
-    select *,
-        'Green' as service_type
-    from {{ ref('stg_green_tripdata') }}
+with trips_unioned as (
+    select * from {{ ref('int_trips_unioned') }}
 ),
-yellow_tripdata as (
-    select *,
-    'Yellow' as service_type
-    from {{ ref('stg_yellow_tripdata') }}
-),
-trips_unioned as (
-    select * from green_tripdata
-        union all 
-    select * from yellow_tripdata
-),
+
 dim_zones as (
     select * from {{ ref('dim_zones') }}
     where borough != 'Unknown'
